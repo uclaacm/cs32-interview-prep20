08ffc40795210dc6ff57c7d75e648767b678a015
Elena <elena@agonswim.com>
2020-01-15 13:40:41 -0800

    headers added for all python problems for workshop 1

---
+++ Workshop1_Introduction/Problem3_Robot Return to Origin_657.py
@@ -0,0 +1,9 @@
+# UCLA ACM ICPC: Interview Track Leetcode Problem Solutions
+
+# Disclaimer: This is not the only valid solution and we do not claim our solutions 
+# to be optimal in terms of runtime or memory usage, we are merely giving example 
+# solutions to questions for learning purposes only
+
+# Remove Robot Return to Origin
+# Leetcode Problem 657
+# https://leetcode.com/problems/robot-return-to-origin/
