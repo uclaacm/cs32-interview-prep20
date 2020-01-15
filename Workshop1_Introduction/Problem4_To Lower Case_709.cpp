b3df3468d87f7e1a8baec75478e19d8f5c17f64b
Elena <elena@agonswim.com>
2020-01-15 13:44:51 -0800

    header added to all workshop 1 solutions

---
+++ Workshop1_Introduction/Problem4_To Lower Case_709.cpp
@@ -0,0 +1,12 @@
+/*
+    UCLA ACM ICPC: Interview Track Leetcode Problem Solutions
+
+    Disclaimer: This is not the only valid solution and we do not claim our solutions 
+    to be optimal in terms of runtime or memory usage, we are merely giving example 
+    solutions to questions for learning purposes only
+
+    To Lower Case
+    Leetcode Problem 709
+    https://leetcode.com/problems/to-lower-case/
+
+ */
---
+++ Workshop1_Introduction/Warmup_Fizz Buzz_412.cpp
@@ -0,0 +1,11 @@
+/*
+    UCLA ACM ICPC: Interview Track Leetcode Problem Solutions
+
+    Disclaimer: This is not the only valid solution and we do not claim our solutions 
+    to be optimal in terms of runtime or memory usage, we are merely giving example 
+    solutions to questions for learning purposes only
+
+    Fizz Buzz
+    Leetcode Problem 412
+    https://leetcode.com/problems/fizz-buzz/
+*/
